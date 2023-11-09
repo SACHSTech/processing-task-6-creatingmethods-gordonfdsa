ef983ae5f535da52aed4afe2789505c74602d7ce
Gordon <gordon.zhu25@ycdsbk>
2023-11-09 09:28:51 -0500

    Final Code

--- Main.java
+++ Main.java
@@ -1,16 +1,17 @@
 import processing.core.PApplet;
 
 /**
- * Draws a parabolic path of houses with different colors 
- * @author: Gordon Z 
- *
+ * Draws parabolic paths of clouds with different colors, and also a grid of
+ * houses underneath with methods
+ * 
+ * @author: Gordon Z
  */
 class Main {
   public static void main(String[] args) {
-    
-    String[] processingArgs = {"MySketch"};
-	  Sketch mySketch = new Sketch();
-	  PApplet.runSketch(processingArgs, mySketch);
+
+    String[] processingArgs = { "MySketch" };
+    Sketch mySketch = new Sketch();
+    PApplet.runSketch(processingArgs, mySketch);
   }
-  
+
 }
--- Sketch.java
+++ Sketch.java
@@ -15,6 +15,9 @@ public class Sketch extends PApplet {
    */
   public void setup() {
     background(3, 248, 252);
+    fill(3, 145, 36);
+    stroke(3, 145, 36);
+    rect(0, 300, 600, 300);
   }
 
   /**
@@ -24,29 +27,57 @@ public class Sketch extends PApplet {
     int intX;
     int intY;
 
-    for(int i = 0 ; i <= 600; i+= 30){
-      intX = i;
-
-      //calculate the Y position of where the hous will be drawn based off X
-      intY = calculateYPosition(intX);
-      //draw the actual house 
-      drawHouse(intX, intY, (int)(0.425 * i), (int)(0.225 * i), (int)(0.125 * i));
+    // draws the houses
+    for (int i = 40; i < 600; i += 40) {
+      for (int j = 340; j < 600; j += 40) {
+        // draw the actual house
+        drawHouse(i, j);
+      }
     }
 
-    for(int i = 0 ; i <= 600; i+= 30){
+    //draws the parabolic clouds
+    for (int i = 0; i <= 600; i += 30) {
       intX = i;
 
-      //calculate the Y position of where the hous will be drawn based off X
+      // calculates the Y position of where it should be drawn
       intY = calculateYPosition(intX);
 
-      //draw the second row of parabolic houses; 
-      intY += 150; //in order to not overlap with the first path
-      drawHouse(intX, intY, (int)(0.425 * i), (int)(0.125 * i), (int)(0.325 * i));
+      // Draws the cloud with color depending on i
+      drawCloud(intX, intY, (int) (0.425 * (600-i)), (int) (0.225 * i), (int) (0.325 * i));
+    }
+
+    //draws the second path   parabolic clouds
+    for (int i = 0; i <= 600; i += 30) {
+      intX = i;
+
+      // calculates the Y position of where it should be drawn
+      intY = calculateYPosition(intX);
+      intY += 50; //To not overlap with the first path
+
+      // Draws the cloud with color depending on i
+      drawCloud(intX, intY, (int) (0.2 * (i)), (int) (0.525 * i), (int) (0.25 * i));
     }
   }
 
   /**
    * Description:draws a house given coordinates for a center
+   * 
+   * @param intCenterX - the X value of center of where the house will the drawn
+   * @param intCenterY - the Y value of center of where the house will the drawn
+   * 
+   * @author: Gordon
+   */
+  public void drawHouse(int intCenterX, int intCenterY) {
+    noStroke();
+    fill(252, 186, 3);
+    rect(intCenterX - 15, intCenterY - 15, 30, 20);
+
+    fill(204, 93, 2);
+    triangle(intCenterX - 20, intCenterY - 15, intCenterX + 20, intCenterY - 15, intCenterX, intCenterY - 30);
+  }
+
+  /**
+   * Description:draws a cloud given coordinates for a center and a color
    * 
    * @param intCenterX - the X value of center of where the house will the drawn
    * @param intCenterY - the Y value of center of where the house will the drawn
@@ -56,25 +87,27 @@ public class Sketch extends PApplet {
    * 
    * @author: Gordon
    */
-  public void drawHouse(int intCenterX, int intCenterY, int intRcolor, int intGcolor, int intBcolor) {
-    stroke(intRcolor, intGcolor, intBcolor);
-    fill(intRcolor, intGcolor, intBcolor);
-
-    rect(intCenterX - 15, intCenterY - 15, 30, 20);
-    triangle(intCenterX - 20, intCenterY - 15, intCenterX + 20, intCenterY - 15, intCenterX, intCenterY - 30);
+  public void drawCloud(int intCenterX, int intCenterY, int intRcolor, int intGcolor, int intBcolor){
+      fill(intRcolor, intGcolor, intBcolor);
+      
+      rect(intCenterX-12, intCenterY-5, 25, 10);
+      ellipse(intCenterX-5, intCenterY-5, 15, 15);
+      ellipse(intCenterX+5, intCenterY-5, 12, 12);
+      ellipse(intCenterX-10, intCenterY, 10, 10);
+      ellipse(intCenterX+10, intCenterY, 10, 10);
   }
 
   /**
-   * Description:draws a house given coordinates for a center
+   * Description: calculates the Y position given X position for a parabolic path
    * 
-   * @param intX - the X value of center of where the house will the drawn
+   * @param intX - the X value of center of where the object will the drawn
    * 
    * @return intY - the Y value calculated based on the path of a quadratic
    * 
    * @author: Gordon
    */
   public int calculateYPosition(int intX) {
-    int intY = (int) (0.005 * Math.pow((intX - 300), 2) + 50);
+    int intY = (int) (0.002 * Math.pow((intX - 300), 2) + 40);
     return intY;
   }
 }
